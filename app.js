9c936fd6c17a7dc4156c73b5873e0f4980b7f2ca
iulian880 <petcuiulian880@gmail.com>
2020-04-03 14:24:20 +0300

    logging the attacks and displaying the log

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
@@ -26,14 +27,24 @@ new Vue({
         },
 
         playerAttacks: function () {
-            this.monsterHealth -= this.calculateDamage(3, 10)
+            var damage = this.calculateDamage(3, 10)
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            })
             if (this.checkWin()) {
                 return
             }
         },
         monsterAttacks: function () {
-            this.playerHealth -= this.calculateDamage(5, 15)
+            var damage = this.calculateDamage(5, 15)
+            this.playerHealth -= damage
             this.checkWin()
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            })
             return
         },
 
